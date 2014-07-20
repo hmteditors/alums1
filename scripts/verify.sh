e5f336c07c57a39487180f2c01848cb626e25590
Neel Smith <dnsmith.neel@gmail.com>
2014-07-19 20:25:11 -0400

    Update text inventory for hmt-12 and verify.sh so for all projects in alums1 so that we can now validate hmt-12 and any other projects with correct text inventories.

--- scripts/verify.sh
+++ scripts/verify.sh
@@ -1,12 +1,14 @@
 
-if [ "$#" -ne 1 ]; then
-    echo "Usage: sh verify.sh URN"
+if [ $# -eq 2 ]; then
+    CONF=/vagrant/alums1/${2}/configs/vm-mom-config.gradle
+elif [ $# -eq 1 ]; then
+    CONF=/vagrant/alums1/configs/vm-mom-config.gradle
+else 
+    echo "Usage: sh verify.sh URN" [SUBDIRECTORY]
     exit
 fi
 
-
-echo "Verifying from configuration in /vagrant/alums1/configs/vm-mom-config.gradle verify"
-
+echo "Verifying from configuration in $CONF"
 
 cd /vagrant/hmt-mom
 
@@ -15,4 +17,4 @@ gradle clean
 
 echo Beginning verification for folio $1
 
-gradle -Pfolio=$1 -Pconf=/vagrant/alums1/configs/vm-mom-config.gradle verify
+gradle -Pfolio=$1 -Pconf=$CONF verify
